04b4716f11c527ed58aec753bbf5e5ff9b07be3b
Alex Castro <acastro917@gmail.com>
2012-01-24 16:31:32 -0500

    Update tracking.php

--- tracking.php
+++ tracking.php
@@ -3,7 +3,7 @@
 // Constants used to access Scraperwiki API.
 define("SCRAPERWIKI_API_URL", "http://api.scraperwiki.com/api/1.0/datastore/sqlite");
 define("SCRAPERWIKI_FORMAT", "jsondict");
-define("SCRAPERWIKI_NAME", "phl-flight-scraper2");
+define("SCRAPERWIKI_NAME", "table1");
 define("SCRAPERWIKI_QUERY", "select%20*%20from%20%60swdata%60%20where%20%60flight_num%60%20%3D%20%22[[flight_num]]%22%20and%20date%20%3D%20%22[[date]]%22%20and%20%60flight_type%60%20%3D%20%22[[direction]]%22");
 
 // Function to fetch JSON for a specific flight from Scrapewiki.
